a4adf540b9078a89a7f27603d48b6561ca6aa17c
DuyLuu <bdl.uit@gmail.com>
2021-07-23 10:09:25 +0700

    Create Todos component

---
+++ src/components/Todos.tsx
@@ -0,0 +1,11 @@
+const Todos = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Learn Javascript</li>
+            <li>Learn Typescript </li>
+        </ul>
+    )
+}
+
+export default Todos
